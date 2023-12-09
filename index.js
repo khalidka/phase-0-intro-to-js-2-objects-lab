70e4ef0cdfde67c28e090f19024516296b0c7879
root <root@DESKTOP-3S5SIJL.localdomain>
2023-12-09 19:21:40 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const employee = {
+  name: "sam",
+  streetAddress: "11 Broadway",
+};
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  return {
+    ...employee,
+    [key]: value,
+  };
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+  const employeeClone = { ...employee };
+  delete employeeClone[key];
+  return employeeClone;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+}
